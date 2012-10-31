e0ea78b2313e8f6ee7cb05a3ab0aca95ad9ff4eb
Harish Gavisiddaiah <harishsid@gmail.com>
2012-10-31 18:45:22 -0400

    local-setup

---
+++ git-setup-history.sh
@@ -0,0 +1,44 @@
+  507  mkdir git-repo
+  508  cd git-repo/
+  509  git clone git@github.com:harishsid/lost-ghost.git
+  510  git clone help
+  511  git help
+  512  clear
+  513  git clone https://github.com/harishsid/lost-ghost.git
+  514  ls
+  515  cd lost-ghost/
+  516  ls
+  517  emacs README.md 
+  518  cd ../
+  519  git comit
+  520  git commit
+  521  pwd
+  522  cd lost-ghost/
+  523  git commit
+  524  git commit -a
+  525  git commit -a
+  526  git commit --amend --reset-author
+  527  git config --global user.name "Harish Gavisiddaiah"
+  528  git config --global user.email harishsid@gmail.com
+  529  ls
+  530  vi README.md 
+  531  git commit -a
+  532  clear
+  533  git config --global core.editor emacs
+  534  git config --llist
+  535  git config --list
+  536  clear
+  537  git status
+  538  git remote
+  539  git remote -v
+  540  git push
+  541  clear
+  542  ls
+  543  emacs git-commands.txt
+  544  ls
+  545  git add git-commands.txt 
+  546  git push
+  547  git commit -a -m "adding help file"
+  548  git push
+  549  history | less
+  550  history > git-setup-history.sh
