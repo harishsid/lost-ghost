61aa7f4dd007e4b6ff3b8a9a88dde3e05368ea38
Harish Gavisiddaiah <harishsid@gmail.com>
2013-01-25 16:13:38 -0500

    Update logic

--- data-struct/src/main/java/org/harry/datastruct/MultiKeyTreeMap.java
+++ data-struct/src/main/java/org/harry/datastruct/MultiKeyTreeMap.java
@@ -3,78 +3,233 @@
  */
 package org.harry.datastruct;
 
+
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 /**
- * @author harry
- *
+ * B-tree concept based Java implementation, provide basic Map functionality with support for multiple keys. Provide
+ * ability to structure large set of data stored in Tree structure and ability to search for both for exact match and
+ * best match.
+ * 
+ * <p>
+ * Implementation has Tree node with multiple child node and default child node. Child nodes are stored in map with
+ * their key or if key is null default child node is used.
+ * 
+ * </p>
+ * 
+ * <p>
+ * Map is initialized with threshold, which defines the depth of Tree. Once threshold is defined, keys
+ * {@link MultiKeyTreeMapKey} used for {@link #get(MultiKeyTreeMapKey)}, {@link #put(MultiKeyTreeMapKey, Object)},
+ * {@link #findBestMatch(MultiKeyTreeMapKey)} operations should return {@link MultiKeyTreeMapKey#getkeys()} list with
+ * size == threshold, else {@link IllegalArgumentException} exception will be thrown.
+ * </p>
+ * 
+ * <p>
+ * Each Tree node represent condition path in tree, based on its level it will be either default node path for its
+ * parent node or path to one of key's value used by its parent node.
+ * </p>
+ * 
+ * <p>
+ * Tree start with {@link #entryNode} which is the root node of the tree and values are always stored in last node in
+ * tree hierarchy. Node position is governed by conditions passed in Key and depth of tree ({@link #threshold}
+ * 
+ * Map put {@link #put(MultiKeyTreeMapKey, V)} operation is condition based sequential node insertion. Keys list is
+ * iterated in sequence and Node insertion starts with root node, for each key if key is null, default node is inserted
+ * else we lookup we look up child map with key if child node not found it insert a child node with key. Insertion is
+ * then delegated to either default node or child node. This operation is continued recursively till all keys are
+ * iterated and value is stored in last node.
+ * 
+ * </p>
+ * 
+ * <p>
+ * Map get operation {@link #get(MultiKeyTreeMapKey)} operation is condition based sequential node navigation. Keys list
+ * is iterated in sequence and Node navigation starts with root node. Navigation logic for using each key it fetch child
+ * node, if key is null it navigates to default child node.
+ * 
+ * Navigation ends if there is there is no child node, else logic is recursed till all keys are iterated and value from
+ * last node is returned.
+ * <p>
+ * 
+ * <p>
+ * Best match {@link #findBestMatch(MultiKeyTreeMapKey)} operation work exactly {@link #get(MultiKeyTreeMapKey)}
+ * operation except that it navigate to default child node both when key is null or if we don't find child node using
+ * key.
+ * 
+ * This feather allows us structure data with multiple conditions and allows us search best possible match using one
+ * key.
+ * 
+ * </p>
+ * 
+ * @author <a href="mailto:a395432@fmr.com">Harish Gavisiddaiah</a>
+ * @since 2013
+ * @param <K> type of keys used.
+ * @param <M> implementation of {@link MultiKeyTreeMapKey} which represent group of keys.
+ * @param <V> type of value stored in nodes.
+ * 
  */
-public class MultiKeyTreeMap<K extends Comparable<K>, M extends MultiKeyTreeMapKey<K>, V> {
+public class MultiKeyTreeMap<K, M extends MultiKeyTreeMapKey<K>, V> {
 
-	public static final String NILL = "*NILL";
-	
-	 /**
+    public static final String NILL = "*NILL";
+
+    /**
      * The number of key-value mappings contained in this map.
      */
-    transient int size;
-    
-    
-    private static final class TreeNode<K, V> {
-    	private V value;
-    	private Map<K, TreeNode<K, V>> childrens = new HashMap<K, MultiKeyTreeMap.TreeNode<K,V>>();
-    	
-    	public V findBestMatch(Iterator<K> keys) {
-    		if (keys.hasNext()) {
-    			
-    		}
-    		return value;
-    	}
-    	
+    private long size;
+
+    /** threshold defines the depth of Tree. */
+    private int threshold;
+
+    /** Root Node. */
+    private TreeNode<K, V> entryNode = new TreeNode<K, V>();
+
+    /**
+     * Default constructor.
+     * 
+     * @param threshold define the depth of the queue which is equal number of keys/conditions used to get/put
+     *            operation.
+     */
+    public MultiKeyTreeMap(int threshold) {
+        this.threshold = threshold;
     }
-    
-	public int size() {
-		return size;
-	}
 
-	public boolean isEmpty() {
-		return size == 0;
-	}
+    /**
+     * Navigate tree nodes till all keys in {@link MultiKeyTreeMapKey} are matched and return value.
+     * 
+     * @param key Multi key.
+     * @return value if found.
+     */
+    public V get(M key) {
+        Iterator<K> keys = validateKeys(key);
+        return entryNode.get(keys);
+    }
 
-	public boolean containsKey(Object key) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    /**
+     * Navigate tree nodes to match or best match all keys in {@link MultiKeyTreeMapKey} and return matched value.
+     * 
+     * @param key Multi key.
+     * @return value if found.
+     */
+    public V findBestMatch(M key) {
+        Iterator<K> keys = validateKeys(key);
+        return entryNode.findBestMatch(keys);
+    }
 
-	public boolean containsValue(Object value) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    /**
+     * Store the value in Tree node which match all keys in {@link MultiKeyTreeMapKey}. Nodes inserted if they don't
+     * exist for given condition.
+     * 
+     * @param key Mult Key.
+     * @param value if found.
+     */
+    public void put(M key, V value) {
+        Iterator<K> keys = validateKeys(key);
+        entryNode.put(keys, value);
+        size++;
+    }
 
-	public V get(Object key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /**
+     * Reset the tree.
+     */
+    public void clear() {
+        // Reset Entry node
+        entryNode = new TreeNode<K, V>();
+        size = 0;
+    }
 
-	public V put(M key, V value) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /**
+     * Returns the number of values stored in Tree Map.
+     * 
+     * Note: This is not number of nodes present in the tree.
+     * 
+     * @return number of values.
+     */
+    public long size() {
+        return size;
+    }
 
-	public V remove(Object key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /**
+     * 
+     * @param key keys to validate.
+     * @return Iterator of keys.
+     */
+    private Iterator<K> validateKeys(M key) {
+        if (key == null || key.getkeys() == null) {
+            throw new NullPointerException();
+        }
+        if (threshold != key.getkeys().size()) {
+            throw new IllegalArgumentException("Invalid Key specified, number conditions/key exceed queue depth "
+                + threshold);
+        }
+        Iterator<K> keys = key.getkeys().iterator();
+        return keys;
+    }
 
-	public void putAll(Map<? extends M, ? extends V> m) {
-		// TODO Auto-generated method stub
-		
-	}
+    /**
+     * Tree Node representation with support for multiple nodes with condition and default node.
+     */
+    private static final class TreeNode<K, V> {
+        private V value;
+        private Map<Object, TreeNode<K, V>> childNodes = new HashMap<Object, TreeNode<K, V>>();
+        private TreeNode<K, V> defaultChildNode;
 
-	public void clear() {
-		// TODO Auto-generated method stub
-		
-	}
+        private V findBestMatch(Iterator<K> keys) {
+            if (keys.hasNext()) {
+                Object key = keys.next();
+                TreeNode<K, V> childNode = defaultChildNode;
+                if (key != null) {
+                    TreeNode<K, V> match = childNodes.get(key);
+                    if (match != null) { // If no found naviagate under Nill condition.
+                        childNode = match;
+                    }
+                }
+                if (childNode == null) {
+                    return null;
+                }
+                return childNode.findBestMatch(keys);
+            } else { // End of tree node.
+                return value;
+            }
+        }
 
+        private V get(Iterator<K> keys) {
+            if (keys.hasNext()) {
+                Object key = keys.next();
+                TreeNode<K, V> childNode = defaultChildNode;
+                if (key != null) { // Always override the default Node if Key is not null for exact match.
+                    childNode = childNodes.get(key);
+                }
+                if (childNode == null) {
+                    return null;
+                }
+                return childNode.get(keys);
+            } else {
+                return value;
+            }
+        }
+
+        private void put(Iterator<K> keys, V value) {
+            if (keys.hasNext()) {
+                Object key = keys.next();
+                if (key != null) {
+                    TreeNode<K, V> childNode = childNodes.get(key);
+                    if (childNode == null) { // Create node if don't exist
+                        childNode = new TreeNode<K, V>();
+                    }
+                    childNodes.put(key, childNode); // Register node with parent.
+                    childNode.put(keys, value);// Recurs till end of Tree.
+                } else { // Null key user defaul Node
+                    if (defaultChildNode == null) {
+                        defaultChildNode = new TreeNode<K, V>();
+                    }
+                    defaultChildNode.put(keys, value);
+                }
+
+            } else { // End of Tree
+                this.value = value;
+            }
+        }
+
+    }
 }
--- data-struct/src/main/java/org/harry/datastruct/MultiKeyTreeMapKey.java
+++ data-struct/src/main/java/org/harry/datastruct/MultiKeyTreeMapKey.java
@@ -3,13 +3,23 @@ package org.harry.datastruct;
 import java.util.List;
 
 /**
- * Define APIs to be implemented by M
+ * Defines type of Keys will be used for {@link MultiKeyTreeMapKey}
  * 
- * @author harry
- *
- * @param <K> type of key
+ * @author <a href="mailto:a395432@fmr.com">Harish Gavisiddaiah</a>
+ * @since 2013
+ * @param <K> type of keys used.
  */
-public interface MultiKeyTreeMapKey<K extends Comparable<K>> {
-	
-  public List<K> getkeys();
+public interface MultiKeyTreeMapKey<K> {
+    
+    /**
+     * Implementation, should return list keys to be used to access map.
+     * 
+     * <p>Size and sequence of keys should always remain, if user wants to satisfy condition map.put(key, value) == map.get(key, value)</p>
+     * 
+     * <p>If some values don't have all keys defined, still key implementation should return list of keys with same size and sequence, except that 
+     * it can have null values in list for key positions to which value not available<p>
+     * 
+     * @return list of all keys.
+     */
+    List<K> getkeys();
 }
